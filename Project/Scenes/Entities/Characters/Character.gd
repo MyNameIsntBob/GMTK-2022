da311a800b8226fe60283ac067af156669326522
MyNameIsntBob <37026012+MyNameIsntBob@users.noreply.github.com>
2022-07-15 17:32:34 -0600

    add health counter

--- Project/Scenes/Entities/Characters/Character.gd
+++ Project/Scenes/Entities/Characters/Character.gd
@@ -14,11 +14,16 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity)
 
 
+func _process(delta):
+	$Health.text = str(health)
+
+
 func shoot():
 	var bullet : Bullet = BULLET.instance()
 	get_parent().add_child(bullet)
 	bullet.global_position = shoot_pos.global_position
 	bullet.velocity = (shoot_pos.global_position - self.global_position) * shoot_speed
+
 
 func damage(amount):
 	health -= amount
