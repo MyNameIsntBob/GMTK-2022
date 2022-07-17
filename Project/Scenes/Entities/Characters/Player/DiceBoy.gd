0df2f7ebfdca526e9d3e11499c2578b2c44bb05d
MyNameIsntBob <37026012+MyNameIsntBob@users.noreply.github.com>
2022-07-17 11:22:09 -0600

    Add aminations (kind of)

--- Project/Scenes/Entities/Characters/Player/DiceBoy.gd
+++ Project/Scenes/Entities/Characters/Player/DiceBoy.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 var animation : String = 'Walk' setget set_animation
+var current_boy
 
 func set_animation(new_animation):
 	print('set_animation')
@@ -12,6 +13,12 @@ func set_animation(new_animation):
 	old_boy.visible = false
 	new_boy.visible = true
 	
-#	old_boy.get_node("AnimationPlayer").stop()
-#	new_boy.get_node('AnimationPlayer').play('default')
+	current_boy = new_boy
+	
+	old_boy.get_node("AnimationPlayer").stop()
+	new_boy.get_node('AnimationPlayer').play('default')
 	animation = new_animation
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	current_boy.play('anim_name')
